c4a853037dab8f8b28182add1b515fcc432e9dc3
bayonadavid10-sudo <bayonadavid10@gmail.com>
2026-06-11 22:52:49 -0500

    Update MongoConfig.java

--- src/main/java/com/example/demo/config/MongoConfig.java
+++ src/main/java/com/example/demo/config/MongoConfig.java
@@ -1,25 +1,18 @@
 package com.example.demo.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-
 @Configuration
 public class MongoConfig {
-
-    private static final String URI = "mongodb://localhost:27017";
-private static final String DATABASE = "tradeglobal";
-
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create(URI);
+        String uri = System.getenv("SPRING_DATA_MONGODB_URI");
+        return MongoClients.create(uri);
     }
-
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), DATABASE);
+        return new MongoTemplate(mongoClient(), "tradeglobal");
     }
 }
